74e55d077de26948bb50ea77f71c44260ebad3a6
krrakshit <rkrkr365@gmail.com>
2025-04-10 17:14:40 +0530

    Remove APITester component and related imports from App. Update API route from "/api/hello" to "/hero" and implement server-side rendering for Hero component. Clean up frontend.tsx by removing unnecessary comments.

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,24 @@
 import { serve } from "bun";
 import index from "./index.html";
+import { renderToReadableStream } from "react-dom/server";
 
 const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
 
-    "/api/hello": {
+    "/hero": {
       async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
+        const {default : Hero} = await import("../src/pages/hero")
+        const stream = await renderToReadableStream(<Hero/>)
+        return new Response (stream,{
+          headers: {
+            "Content-Type": "text/html",
+          },
+        })
+      }
     },
-
-    "/api/hello/:name": async (req) => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
+    
   },
 
   development: process.env.NODE_ENV !== "production",
